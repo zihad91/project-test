aaa43cb2fb26244462e2da1b845fb48db9492d24
zihad91 <137558351+zihad91@users.noreply.github.com>
2025-08-21 23:54:51 +0600

    Update main2.py

--- main2.py
+++ main2.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/message")
 def massage():
-    return {"message": "Hello, this is a massage endpoint!"}
+    return {"message": "Hello, this is a message from the FastAPI app!"}
